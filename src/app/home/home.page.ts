9fbd7ae7f8e186848cf3e098df1962893cd7d22d
Bryant Plano <bryant@ionic.io>
2019-09-27 12:52:49 -0500

    Repro the issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Plugins } from '@capacitor/core';
+const { Keyboard } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  public loginForm;
 
-  constructor() {}
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+  ngOnInit() {
+    this.loginForm = this.buildLoginForm();
+    Keyboard.setAccessoryBarVisible({isVisible: true});
+  }
+
+  private buildLoginForm(): FormGroup {
+    return this.formBuilder.group({
+      username: [''],
+      password: [''],
+    });
+  }
+
 
 }
